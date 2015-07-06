8ef42a51476090b7c4fb665dcaaee3a9957e2a9c
jairophp <guitarristajairo@gmail.com>
2015-07-05 22:36:53 -0300

    configuracoes prontas cap1

---
+++ application/views/common/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jairo
+ * Date: 05/07/15
+ * Time: 21:56
+ */
+?>
+</div> <!-- /container -->
+<link href="<?php echo base_url('bootstrap/css/bootstrap.min.
+css'); ?>" rel="stylesheet">
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- add no fim do documento prar que as pagina carregem mas rapido -->
+<script src="https://ajax.googleapis.com/ajax/
+libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="<?php echo base_url
+('bootstrap/js/bootstrap.min.js');?>"></script>
+<script src="<?php echo base_url
+('bootstrap/js/docs.min.js');?>"></script>
+</body>
+</html>
